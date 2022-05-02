0a451a0f0f9ae9d4074d6a7739b850a9a59c9c92
simon.bridgeford@cloudcall.com <simon.bridgeford@cloudcall.com>
2022-05-02 11:21:18 +0100

    change folder name, add SSM

---
+++ StravaStreetViewTokenLambda/index.js
@@ -0,0 +1,81 @@
+const fetch = require('node-fetch')
+var AWS = require('aws-sdk')
+AWS.config.update({ region: 'eu-west-1' });
+var ssm = new AWS.SSM();
+
+const client_id = "mapillary_ClientId";
+const client_secret = "mapillary_ClientSecret";
+const access_token = "mapillary_AccessToken";
+
+exports.handler = async (event, context, callback) => {
+    console.log("event/n" + JSON.stringify(event));
+
+    var url = "https://graph.mapillary.com/token";
+    var code = event.queryStringParameters.code;
+
+    console.log("code is " + code);
+
+    var clientId = await getSSMParameter(client_id);
+    var secret = await getSSMParameter(client_secret);
+
+    var data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "client_id": clientId
+    }
+
+    const apiResponse = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "OAuth "+ secret
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+
+    console.log("awaiting api response");
+
+    var res = await apiResponse.json();
+
+    console.log("api response is " + JSON.stringify(res));
+
+    if(res.access_token)
+        storeSSMParameter(access_token, res.access_token);
+
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(res),
+    };
+
+    callback(null, response);
+};
+
+async function getSSMParameter(name) {
+    let parameter = {
+        Name: name,
+        WithDecryption: false
+      };
+
+    const data = await ssm.getParameter(parameter).promise();
+
+    console.log(data);
+    console.log(data.Parameter.Value);
+
+    return data.Parameter.Value;
+}
+
+async function storeSSMParameter(key, value){
+
+    let parameter = {
+        Name: key,
+        Value: value,
+        Overwrite: true
+    }
+
+    const result = await ssm.putParameter(parameter).promise();
+
+    console.log(result);
+
+    return result;
+}
